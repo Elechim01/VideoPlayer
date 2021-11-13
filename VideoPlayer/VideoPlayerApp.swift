7dd04fd748e96a87394b21d0783e8591dc083cb4
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-11-14 00:09:08 +0100

    Initial Commit

---
+++ VideoPlayer/VideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerApp.swift
+//  VideoPlayer
+//
+//  Created by Michele Manniello on 14/11/21.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
